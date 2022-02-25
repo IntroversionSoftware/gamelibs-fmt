03a8606a99c389d0ebb4af1ce22912c438c3d49c
Steven Noonan <steven@uplinklabs.net>
2022-02-25 06:31:55 -0800

    build: adding build system for game builds
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- include/fmt/core.h
+++ include/fmt/core.h
@@ -235,7 +235,8 @@
 #  if defined(__cpp_nontype_template_args) &&                  \
       ((FMT_GCC_VERSION >= 903 && FMT_CPLUSPLUS >= 201709L) || \
        __cpp_nontype_template_args >= 201911L) &&              \
-      !defined(__NVCOMPILER) && !defined(__LCC__)
+      !defined(__NVCOMPILER) && !defined(__LCC__) &&           \
+      !defined(__INTELLISENSE__)
 #    define FMT_USE_NONTYPE_TEMPLATE_ARGS 1
 #  else
 #    define FMT_USE_NONTYPE_TEMPLATE_ARGS 0
